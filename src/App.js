2c5948789afc9fde76ccd677f60073a7f1908600
Rishi4702 <golurishav26@gmail.com>
2023-04-06 16:00:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from "react";
+const owl = {
+  title: 'Excellent Owl',
+  src: 'https://content.codecademy.com/courses/React/react_photo-owl.jpg'
+};
+
+class Rishav extends React.Component {
+  render() {
+    return (
+      <div>
+        <blockquote>
+          <p>What is important now is to recover our senses.</p>
+          <cite>
+            <a
+              target="_blank"
+              href="https://en.wikipedia.org/wiki/Susan_Sontag"
+            >
+              Susan Sontag
+            </a>
+          </cite>
+        </blockquote>
+      </div>
+    );
+  }
+}
+
+class Owl extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{owl.title}</h1>
+        <img 
+          src={owl.src}
+          alt={owl.title}
+        />
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Rishav/>
+        <Owl/>
     </div>
   );
 }
